18aa3c146ba134c3f5d8d7b3f1fd0a8eee0ac110
Sergio Gonzalez <sergiomanuel2020@gmail.com>
2020-12-02 00:48:31 -0600

    Create CPP Proyect

---
+++ Proyecto Integrador Primer semestre OOP/Proyecto Integrador Primer semestre OOP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Proyecto Integrador Primer semestre OOP
+//
+//  Created by Sergio Gonzalez on 02/12/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
